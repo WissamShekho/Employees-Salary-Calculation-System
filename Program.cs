ac6c75bc4a63103dd49ec064241c9f8223df0ce7
Wissam Shekho <Shekhowesam91@gmail.com>
2025-05-31 10:57:30 +0300

    Apply Polymorphism

--- Program.cs
+++ Program.cs
@@ -5,16 +5,20 @@
         static void Main(string[] args)
         {
             Manager manager = new Manager(101, "Muhammed", "Khalid", 50);
-            Console.WriteLine(manager.SalaryReport());
+            // Console.WriteLine(manager.SalaryReport());
 
             SalesAgent salesAgent = new SalesAgent(102, "Fadi", "Abdullah", 20, 10_000);
-            Console.WriteLine(salesAgent.SalaryReport());
+            // Console.WriteLine(salesAgent.SalaryReport());
 
             Handyman handyman = new Handyman(1, "Khalid", "Sami", 51);
-            Console.WriteLine(handyman.SalaryReport());
+            // Console.WriteLine(handyman.SalaryReport());
 
             SoftwareEngineer softwareEngineer = new SoftwareEngineer(1, "Ahmed", "Sabri", 42, 10);
-            Console.WriteLine(softwareEngineer.SalaryReport());
+            // Console.WriteLine(softwareEngineer.SalaryReport());
+            
+            List<Employee> Employees = [manager, salesAgent, handyman, softwareEngineer]; 
+
+            Employees.ForEach(employee => Console.WriteLine(employee.SalaryReport())); // polymorphically
 
             Console.ReadKey();
         }
